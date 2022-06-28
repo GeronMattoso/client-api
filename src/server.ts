80b24d3a7c7785bc3cdcf7b422359afab7695b43
Geron Mattoso <gwdcmattoso@inf.ufpel.edu.br>
2022-06-27 22:19:42 -0300

    create the server

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import { App } from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+new App().server.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
